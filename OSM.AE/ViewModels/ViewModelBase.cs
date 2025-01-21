ac6ac71526b1bfe453b1fbf2caa0ac8dbb4f49ad
anton <anton@DESKTOP-SQF6NS3>
2025-01-21 23:09:35 +0500

    Добавьте файлы проекта.

---
+++ OSM.AE/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace OSM.AE.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ OSM.AE/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace OSM.AE.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
